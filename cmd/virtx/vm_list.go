fe544bf94f951ca52a8704700c3120c6436d47d1
Claudio Fontana <cfontana@suse.de>
2025-08-08 17:21:52 +0200

    cmd/virtx: vm_list implementation
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- cmd/virtx/vm_list.go
+++ cmd/virtx/vm_list.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"suse.com/virtx/pkg/model"
 )
 
 func vm_list(list *openapi.VmList) {
+
+	fmt.Fprintf(virtx.w, "UUID\tNAME\tHOST\tSTATE\tVLANID\t \n")
+
+	for _, item := range (list.Items) {
+		fmt.Fprintf(virtx.w, "%s\t%s\t%s\t%s\t%6d\t%v\n", item.Uuid, item.Fields.Name, item.Fields.Host,
+			item.Fields.Runstate.String(), item.Fields.Vlanid, item.Fields.Custom)
+	}
 }
