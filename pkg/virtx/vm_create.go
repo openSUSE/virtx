18ffcbf07c86ff53e737a249463b8275a041a840
Claudio Fontana <cfontana@suse.de>
2025-08-09 19:38:33 +0200

    virtx: vm_create, vm_update: return 201 Created
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- pkg/virtx/vm_create.go
+++ pkg/virtx/vm_create.go
@@ -83,6 +83,6 @@ func vm_create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusCreated)
 	w.Write(buf.Bytes())
 }
--- pkg/virtx/vm_update.go
+++ pkg/virtx/vm_update.go
@@ -120,6 +120,6 @@ func vm_update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusCreated)
 	w.Write(buf.Bytes())
 }
