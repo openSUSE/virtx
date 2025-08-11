7eeeaa59433c27a5a989dec8f56e685894ef5472
Claudio Fontana <cfontana@suse.de>
2025-08-11 13:55:11 +0200

    cmd/virtx: vm_runstate_get: implement
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- cmd/virtx/vm_runstate_get.go
+++ cmd/virtx/vm_runstate_get.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"suse.com/virtx/pkg/model"
 )
 
 func vm_runstate_get(runinfo *openapi.Vmruninfo) {
+	fmt.Fprintf(virtx.w, "HOST\tSTATE\n")
+	fmt.Fprintf(virtx.w, "%s\t%s\n", runinfo.Host, runinfo.Runstate)
 }
