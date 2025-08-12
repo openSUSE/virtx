81a19327013081aa657cf12fb016aa0d80652845
Claudio Fontana <cfontana@suse.de>
2025-08-12 15:28:26 +0200

    virtx: vm_migrate: error while deleting old definition is not fatal
    turn an error return into a Warning header for the user,
    if the old definition is still there after a migration.
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- pkg/virtx/vm_migrate.go
+++ pkg/virtx/vm_migrate.go
@@ -102,7 +102,21 @@ func vm_migrate_offline(host_new string, host_old string, uuid_old string, w htt
 		http.Error(w, "could not define VM", http.StatusInternalServerError)
 		return
 	}
-	/* host_old is remote, we have to request the old host to delete the old VM */
+	/*
+	 * new domain is created, now we have to request the old host to delete
+	 * the old domain. But if we fail to do so, two domains might exist at the
+	 * same time. It would be tempting to then delete the new domain,
+	 * but that could fail too, or we might be getting an HTTP error
+	 * unrelated to the application, when the domain has been deleted,
+	 * leading to the VM being lost.
+	 *
+	 * This is roughly the same for live migration, a problem that libvirt
+	 * itself has to deal with, and it also chooses to err on the safe
+	 * side, and have two domain definitions (one inactive, one active)
+	 * rather than risking losing the VM.
+	 *
+	 * Warn the user with appropriate headers.
+	 */
 	var (
 		delete_opts openapi.VmDeleteOptions
 		buf bytes.Buffer
@@ -111,26 +125,26 @@ func vm_migrate_offline(host_new string, host_old string, uuid_old string, w htt
 	delete_opts.Deletestorage = false
 	err = json.NewEncoder(&buf).Encode(&delete_opts)
 	if (err != nil) {
-		http.Error(w, "failed to encode JSON", http.StatusInternalServerError)
-		return
+		logger.Log("failed to encode JSON for DELETE")
+		w.Header().Set("Warning", `299 VirtX "old domain could not be deleted"`)
+		goto done
 	}
 	response, err = http_do_request(host_old, "DELETE",	fmt.Sprintf("/vms/%s", uuid_old), &delete_opts)
 	if (err != nil) {
 		logger.Log("do_request failed: %s", err.Error())
-		http.Error(w, "failed to request deletion", http.StatusInternalServerError)
-		return
+		w.Header().Set("Warning", `299 VirtX "old domain could not be deleted"`)
+		goto done
 	}
 	defer response.Body.Close()
 	if (response.StatusCode < 200 || response.StatusCode > 299) {
 		logger.Log("do_request failed, status %s", response.Status)
-		http.Error(w, "failed to request deletion", http.StatusInternalServerError)
-		return
+		w.Header().Set("Warning", `299 VirtX "old domain could not be deleted"`)
 	}
+done:
 	buf.Reset()
 	err = json.NewEncoder(&buf).Encode(&uuid_new)
 	if (err != nil) {
-		http.Error(w, "failed to encode JSON", http.StatusInternalServerError)
-		return
+		logger.Log("failed to encode JSON")
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
