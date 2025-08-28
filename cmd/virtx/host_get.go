d983d315019eb4d59935ac1553bc0f778f17b0d8
Claudio Fontana <cfontana@suse.de>
2025-08-28 16:39:28 +0200

    cmd/virtx: host_get: add MEMORY_USED_VM statistic
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- cmd/virtx/host_get.go
+++ cmd/virtx/host_get.go
@@ -7,13 +7,14 @@ import (
 
 func host_get(host *openapi.Host) {
 	if (virtx.stat) {
-		fmt.Fprintf(virtx.w, "  CPU_TOTAL\t   CPU_USED\t   CPU_FREE\tCPU_USED_OS\tCPU_USED_VM\t CPU_RES_VM\t CPU_AVL_VM\t  MEM_TOTAL\t   MEM_USED\t   MEM_FREE\tMEM_USED_OS\t MEM_RES_VM\t MEM_AVL_VM\n")
-		fmt.Fprintf(virtx.w, "%7d MHz\t%7d MHz\t%7d MHz\t%7d MHz\t%7d MHz\t%7d MHz\t%7d MHz\t%7d MiB\t%7d MiB\t%7d MiB\t%7d MiB\t%7d MiB\t%7d MiB\n",
+		fmt.Fprintf(virtx.w, "  CPU_TOTAL\t   CPU_USED\t   CPU_FREE\tCPU_USED_OS\tCPU_USED_VM\t CPU_RES_VM\t CPU_AVL_VM\t  MEM_TOTAL\t   MEM_USED\t   MEM_FREE\tMEM_USED_OS\tMEM_USED_VM\t MEM_RES_VM\t MEM_AVL_VM\n")
+		fmt.Fprintf(virtx.w, "%7d MHz\t%7d MHz\t%7d MHz\t%7d MHz\t%7d MHz\t%7d MHz\t%7d MHz\t%7d MiB\t%7d MiB\t%7d MiB\t%7d MiB\t%7d MiB\t%7d MiB\t%7d MiB\n",
 			host.Resources.Cpu.Total, host.Resources.Cpu.Used, host.Resources.Cpu.Free,
 			host.Resources.Cpu.Usedos, host.Resources.Cpu.Usedvms,
 			host.Resources.Cpu.Reservedvms, host.Resources.Cpu.Availablevms,
 			host.Resources.Memory.Total, host.Resources.Memory.Used, host.Resources.Memory.Free,
-			host.Resources.Memory.Usedos, host.Resources.Memory.Reservedvms, host.Resources.Memory.Availablevms,
+			host.Resources.Memory.Usedos, host.Resources.Memory.Usedvms,
+			host.Resources.Memory.Reservedvms, host.Resources.Memory.Availablevms,
 		)
 	} else {
 		fmt.Fprintf(virtx.w, "NAME\tCPU\tVENDOR\tMODEL\tSOCKS\tCORES\tTH\tTSC_FREQ\tFWVER\tFWDATE\tSTATE\n")
