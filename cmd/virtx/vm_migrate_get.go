56cae7bcfe19e2092f3411555e4078abbd37bf4f
Claudio Fontana <cfontana@suse.de>
2025-09-28 13:25:43 +0200

    cmd: virtx: vm_migrate_get implementation
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- cmd/virtx/vm_migrate_get.go
+++ cmd/virtx/vm_migrate_get.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"suse.com/virtx/pkg/model"
 )
 
 func vm_migrate_get(info *openapi.MigrationInfo) {
+	var p *openapi.MigrationProgress = &info.Progress
+	fmt.Fprintf(virtx.w, "STATE\tRAM TOTAL\tTRANSFERRED\tREMAINING\tRATE\n")
+	fmt.Fprintf(virtx.w, "%s\t%d\t%d\t%d\t%f\n", info.State,
+		p.Total, p.Transferred, p.Remaining, p.Rate)
 }
