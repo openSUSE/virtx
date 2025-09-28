openSUSE/virtx

commit 393ab4efb93bac2def024d5a3bd9f57c01afd437

httpx: new package, extracted from pkg/virtx/http.go
this is useful to share the http client code between
the cli and the virtxd daemon.

Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/virtx/vm_resume.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"suse.com/virtx/pkg/hypervisor"
 	"suse.com/virtx/pkg/logger"
+	"suse.com/virtx/pkg/httpx"
 )
 
 func vm_resume(w http.ResponseWriter, r *http.Request) {
@@ -12,8 +13,14 @@ func vm_resume(w http.ResponseWriter, r *http.Request) {
 	var (
 		err error
 		uuid string
-		vr VirtxRequest
+		vr httpx.Request
 	)
+	_, err = httpx.Decode_request_body(r, nil)
+	if (err != nil) {
+		logger.Log(err.Error())
+		http.Error(w, "failed to decode body", http.StatusBadRequest)
+		return
+	}
 	uuid = r.PathValue("uuid")
 	if (uuid == "") {
 		http.Error(w, "could not get uuid", http.StatusBadRequest)

pkg/httpx/httpx.go
@@ -1,0 +1,208 @@
+/*
+ * Copyright (c) 2024-2025 SUSE LLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>
+ */
+package httpx
+
+import (
+	"net"
+	"net/http"
+	"net/url"
+	"errors"
+	"encoding/json"
+	"bytes"
+	"io"
+	"time"
+
+	"suse.com/virtx/pkg/logger"
+	. "suse.com/virtx/pkg/constants"
+)
+/*
+ * every handler needs to call this, whether it needs to read a body or not,
+ * since the request might contain a body to read and ignore, but the body
+ * needs to be .Close()d because unfortunately r.Body is an io.ReadCloser().
+ * Otherwise connections may stay open.
+ */
+type Request struct {
+	r *http.Request
+	body []byte
+}
+type Response struct {
+	r *http.Response
+	body []byte
+}
+
+const (
+	CLIENT_TIMEOUT = 10
+	CLIENT_IDLE_CONN_MAX = 100
+	CLIENT_IDLE_CONN_MAX_PER_HOST = 10
+	CLIENT_IDLE_TIMEOUT = 15
+	CLIENT_TLS_TIMEOUT = 5
+)
+
+var client http.Client = http.Client{
+	Timeout: CLIENT_TIMEOUT * time.Second,
+	Transport: &http.Transport{
+		MaxIdleConns: CLIENT_IDLE_CONN_MAX,
+		MaxIdleConnsPerHost: CLIENT_IDLE_CONN_MAX_PER_HOST,
+		IdleConnTimeout: CLIENT_IDLE_TIMEOUT * time.Second,
+		TLSHandshakeTimeout: CLIENT_TLS_TIMEOUT * time.Second,
+	},
+}
+
+func Decode_request_body(r *http.Request, arg any) (Request, error) {
+	var (
+		err error
+		vr Request
+	)
+	vr.r = r
+	defer r.Body.Close()
+	if (arg == nil) {
+		return vr, nil
+	}
+	if (r.ContentLength <= 0) {
+		return vr, errors.New("Body expected but not found")
+	}
+	if (r.ContentLength >= HTTP_MAX_BODY_LEN) {
+		return vr, errors.New("content-length exceeded")
+	}
+	vr.body, err = io.ReadAll(io.LimitReader(r.Body, HTTP_MAX_BODY_LEN))
+	if (err != nil) {
+		return vr, errors.New("failed to read body")
+	}
+	if (int64(len(vr.body)) > r.ContentLength) {
+		return vr, errors.New("body len exceeds content-length")
+	}
+	err = json.NewDecoder(bytes.NewReader(vr.body)).Decode(arg)
+	if (err != nil) {
+		return vr, err
+	}
+	return vr, nil
+}
+
+func Decode_response_body(r *http.Response, result any) (Response, error) {
+	var (
+		err error
+		vr Response
+	)
+	vr.r = r
+	defer r.Body.Close()
+	if (result == nil) {
+		return vr, nil
+	}
+	if (r.ContentLength <= 0) {
+		return vr, errors.New("Body expected but not found")
+	}
+	if (r.ContentLength >= HTTP_MAX_BODY_LEN) {
+		return vr, errors.New("content-length exceeded")
+	}
+	if (r.StatusCode >= 200 && r.StatusCode <= 299) {
+		vr.body, err = io.ReadAll(io.LimitReader(r.Body, HTTP_MAX_BODY_LEN))
+		if (err != nil) {
+			return vr, errors.New("failed to read body")
+		}
+		if (int64(len(vr.body)) > r.ContentLength) {
+			return vr, errors.New("body len exceeds content-length")
+		}
+		err = json.NewDecoder(bytes.NewReader(vr.body)).Decode(result)
+	}
+	return vr, err
+}
+
+func Do_request(api_server string, method string, path string, arg any) (*http.Response, error) {
+	var (
+		addr url.URL
+		buf bytes.Buffer
+		err error
+	)
+	addr.Path = path
+	addr.Host = api_server + ":8080"
+	addr.Scheme = "http"
+	err = json.NewEncoder(&buf).Encode(arg)
+	if (err != nil) {
+		return nil, err
+	}
+	req, err := http.NewRequest(method, addr.String(), &buf)
+	if (err != nil) {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	return resp, err
+}
+
+func Proxy_request(api_server string, w http.ResponseWriter, vr Request) {
+	var (
+		newaddr url.URL
+		err error
+	)
+	if (vr.r.Header.Get("X-VirtX-Loop") != "") {
+		logger.Log("proxy_request loop detected")
+		http.Error(w, "loop detected", http.StatusLoopDetected)
+		return
+	}
+	newaddr = *vr.r.URL
+	newaddr.Host = api_server + ":8080"
+	if (vr.r.TLS != nil) {
+		newaddr.Scheme = "https"
+	} else {
+		newaddr.Scheme = "http"
+	}
+	proxyreq, err := http.NewRequest(vr.r.Method, newaddr.String(), bytes.NewReader(vr.body))
+	if (err != nil) {
+		logger.Log("proxy_request http.NewRequest failed: %s", err.Error())
+		http.Error(w, "failed to forward request", http.StatusInternalServerError)
+		return
+	}
+	proxyreq.Header = vr.r.Header.Clone()
+	client_ip, _, err := net.SplitHostPort(vr.r.RemoteAddr)
+	if (err != nil) {
+		logger.Log("proxy_request could not decode client address")
+		http.Error(w, "failed to forward request", http.StatusInternalServerError)
+		return
+	}
+	xff := proxyreq.Header.Get("X-Forwarded-For")
+	if (xff != "") {
+		xff = xff + ", " + client_ip
+	} else {
+		xff = client_ip
+	}
+	proxyreq.Header.Set("X-Forwarded-For", xff)
+	proxyreq.Header.Set("X-VirtX-Loop", "1")
+
+	resp, err := client.Do(proxyreq)
+	if (err != nil) {
+		logger.Log("proxy_request failed: %s", err.Error())
+		http.Error(w, "failed to forward request", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	for name, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(name, value)
+		}
+	}
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+}
+
+func Shutdown() {
+	transport, ok := client.Transport.(*http.Transport)
+	if (ok) {
+		transport.CloseIdleConnections()
+	}
+}

pkg/virtx/host_get.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"bytes"
 	"suse.com/virtx/pkg/model"
+	"suse.com/virtx/pkg/httpx"
+	"suse.com/virtx/pkg/logger"
 )
 
 func host_get(w http.ResponseWriter, r *http.Request) {
@@ -16,6 +18,12 @@ func host_get(w http.ResponseWriter, r *http.Request) {
 		host openapi.Host
 		buf bytes.Buffer
 	)
+	_, err = httpx.Decode_request_body(r, nil)
+	if (err != nil) {
+		logger.Log(err.Error())
+		http.Error(w, "failed to decode body", http.StatusBadRequest)
+		return
+	}
 	uuid = r.PathValue("uuid")
 	if (uuid == "") {
 		http.Error(w, "could not get uuid", http.StatusBadRequest)

pkg/virtx/host_list.go
@@ -5,10 +5,10 @@ import (
 	"encoding/json"
 	"strings"
 	"bytes"
-	"io"
 
-	//	"suse.com/virtx/pkg/logger"
+	"suse.com/virtx/pkg/logger"
 	"suse.com/virtx/pkg/model"
+	"suse.com/virtx/pkg/httpx"
 )
 
 func host_list(w http.ResponseWriter, r *http.Request) {
@@ -21,9 +21,10 @@ func host_list(w http.ResponseWriter, r *http.Request) {
 		host_list openapi.HostList
 		buf bytes.Buffer
 	)
-	err = json.NewDecoder(r.Body).Decode(&o)
-	if (err != nil && err != io.EOF) {
-		http.Error(w, "Failed to decode JSON in Request Body", http.StatusBadRequest)
+	_, err = httpx.Decode_request_body(r, &o)
+	if (err != nil) {
+		logger.Log(err.Error())
+		http.Error(w, "failed to decode body", http.StatusBadRequest)
 		return
 	}
 	/* filters: [name, cpuarch, cpudef, hoststate, memoryavailable] */

pkg/virtx/http.go
@@ -1,83 +1,13 @@
-/*
- * Copyright (c) 2024-2025 SUSE LLC
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see
- * <https://www.gnu.org/licenses/>
- */
 package virtx
 
 import (
-	"net"
 	"net/http"
-	"net/url"
 	"errors"
-	"encoding/json"
-	"bytes"
-	"io"
 
-	"suse.com/virtx/pkg/logger"
 	"suse.com/virtx/pkg/hypervisor"
+	"suse.com/virtx/pkg/httpx"
 	"suse.com/virtx/pkg/model"
-	. "suse.com/virtx/pkg/constants"
 )
-
-/*
- * every handler needs to call this, whether it needs to read a body or not,
- * since the request might contain a body to read and ignore, but the body
- * needs to be .Close()d because unfortunately r.Body is an io.ReadCloser().
- * Otherwise connections may stay open.
- */
-
-type VirtxRequest struct {
-	r *http.Request
-	body []byte
-}
-
-func http_decode_body(r *http.Request, arg any) (VirtxRequest, error) {
-	var (
-		err error
-		vr VirtxRequest
-	)
-	vr.r = r
-	if (r.Body == nil) {     /* no body found */
-		if (arg == nil) {
-			return vr, nil       /* ok, did not expect any */
-		}
-		return vr, errors.New("no body")
-	}
-	if (r.ContentLength <= 0) {
-		r.Body.Close()
-		return vr, errors.New("content-length <= 0")
-	}
-	if (r.ContentLength >= HTTP_MAX_BODY_LEN) {
-		r.Body.Close()
-		return vr, errors.New("content-length exceeded")
-	}
-	vr.body, err = io.ReadAll(io.LimitReader(r.Body, HTTP_MAX_BODY_LEN))
-	r.Body.Close()
-	if (err != nil) {
-		return vr, errors.New("failed to read body")
-	}
-	if (int64(len(vr.body)) > r.ContentLength) {
-		return vr, errors.New("body len exceeds content-length")
-	}
-	err = json.NewDecoder(bytes.NewReader(vr.body)).Decode(arg)
-	if (err != nil) {
-		return vr, err
-	}
-	return vr, nil
-}
 
 func http_host_is_remote(uuid string) bool {
 	return uuid != "" && uuid != hypervisor.Uuid()
@@ -87,102 +17,34 @@ func http_do_request(uuid string, method string, path string, arg any) (*http.Re
 	/* assert (service.m.isRLocked()) */
 	var (
 		host openapi.Host
-		addr url.URL
 		ok bool
-		buf bytes.Buffer
 		err error
+		resp *http.Response
 	)
 	host, ok = service.hosts[uuid]
 	if (!ok) {
 		return nil, errors.New("unknown host")
 	}
-	addr.Path = path
-	addr.Host = host.Def.Name + ":8080"
-	addr.Scheme = "http"
-	err = json.NewEncoder(&buf).Encode(arg)
-	if (err != nil) {
-		return nil, err
-	}
-	req, err := http.NewRequest(method, addr.String(), &buf)
-	if (err != nil) {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/json")
-
 	/* we unlock here to allow other goroutines to make progress while we wait for the response */
 	service.m.RUnlock()
-	resp, err := service.client.Do(req)
+	resp, err = httpx.Do_request(host.Def.Name, method, path, arg)
 	service.m.RLock()
-	if (err != nil) {
-		return nil, err
-	}
-	return resp, nil
+	return resp, err
 }
 
-func http_proxy_request(uuid string, w http.ResponseWriter, vr VirtxRequest) {
+func http_proxy_request(uuid string, w http.ResponseWriter, vr httpx.Request) {
 	/* assert (service.m.isRLocked()) */
 	var (
 		host openapi.Host
-		newaddr url.URL
 		ok bool
-		err error
 	)
 	host, ok = service.hosts[uuid]
 	if (!ok) {
 		http.Error(w, "unknown host", http.StatusUnprocessableEntity)
 		return
 	}
-	if (vr.r.Header.Get("X-VirtX-Loop") != "") {
-		logger.Log("proxy_request loop detected")
-		http.Error(w, "loop detected", http.StatusLoopDetected)
-		return
-	}
-	newaddr = *vr.r.URL
-	newaddr.Host = host.Def.Name + ":8080"
-	if (vr.r.TLS != nil) {
-		newaddr.Scheme = "https"
-	} else {
-		newaddr.Scheme = "http"
-	}
-	proxyreq, err := http.NewRequest(vr.r.Method, newaddr.String(), bytes.NewReader(vr.body))
-	if (err != nil) {
-		logger.Log("proxy_request http.NewRequest failed: %s", err.Error())
-		http.Error(w, "failed to forward request", http.StatusInternalServerError)
-		return
-	}
-
-	proxyreq.Header = vr.r.Header.Clone()
-	client_ip, _, err := net.SplitHostPort(vr.r.RemoteAddr)
-	if (err != nil) {
-		logger.Log("proxy_request could not decode client address")
-		http.Error(w, "failed to forward request", http.StatusInternalServerError)
-		return
-	}
-	xff := proxyreq.Header.Get("X-Forwarded-For")
-	if (xff != "") {
-		xff = xff + ", " + client_ip
-	} else {
-		xff = client_ip
-	}
-	proxyreq.Header.Set("X-Forwarded-For", xff)
-	proxyreq.Header.Set("X-VirtX-Loop", "1")
-
 	/* we unlock here to allow other goroutines to make progress while we wait for the response */
 	service.m.RUnlock()
-	resp, err := service.client.Do(proxyreq)
+	httpx.Proxy_request(host.Def.Name, w, vr)
 	service.m.RLock()
-
-	if (err != nil) {
-		logger.Log("proxy_request failed: %s", err.Error())
-		http.Error(w, "failed to forward request", http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-	for name, values := range resp.Header {
-		for _, value := range values {
-			w.Header().Add(name, value)
-		}
-	}
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
 }

pkg/virtx/virtx.go
@@ -23,22 +23,14 @@ import (
 	"sync"
 	"errors"
 	"context"
-	"time"
 
 	g_uuid "github.com/google/uuid"
 
 	"suse.com/virtx/pkg/logger"
 	"suse.com/virtx/pkg/hypervisor"
 	"suse.com/virtx/pkg/model"
+	"suse.com/virtx/pkg/httpx"
 	. "suse.com/virtx/pkg/constants"
-)
-
-const (
-	CLIENT_TIMEOUT = 10
-	CLIENT_IDLE_CONN_MAX = 100
-	CLIENT_IDLE_CONN_MAX_PER_HOST = 10
-	CLIENT_IDLE_TIMEOUT = 15
-	CLIENT_TLS_TIMEOUT = 5
 )
 
 type Vmdata map[string]hypervisor.Vmdata
@@ -47,7 +39,6 @@ type Hosts map[string]openapi.Host
 type Service struct {
 	servemux *http.ServeMux
 	server http.Server
-	client http.Client
 	m      sync.RWMutex
 
 	cluster openapi.Cluster
@@ -89,15 +80,6 @@ func Init() {
 			Addr: ":8080",
 			Handler: servemux,
 		},
-		client: http.Client{
-			Timeout: CLIENT_TIMEOUT * time.Second,
-			Transport: &http.Transport{
-				MaxIdleConns: CLIENT_IDLE_CONN_MAX,
-				MaxIdleConnsPerHost: CLIENT_IDLE_CONN_MAX_PER_HOST,
-				IdleConnTimeout: CLIENT_IDLE_TIMEOUT * time.Second,
-				TLSHandshakeTimeout: CLIENT_TLS_TIMEOUT * time.Second,
-			},
-		},
 		m:         sync.RWMutex{},
 		cluster:   openapi.Cluster{},
 		hosts:     make(Hosts),
@@ -121,17 +103,9 @@ func New_uuid() string {
 func Shutdown(ctx context.Context) error {
 	var err error
 	err = service.server.Shutdown(ctx)
-	if (err != nil) {
-		return err
-	}
-	transport, ok := service.client.Transport.(*http.Transport)
-	if (ok) {
-		transport.CloseIdleConnections()
-	}
-	if (err != nil) {
-		return err
-	}
-	return nil
+	/* Shutdown the client too (used for proxy) */
+	httpx.Shutdown()
+	return err
 }
 
 func Close() error {

pkg/virtx/vm_boot.go
@@ -3,6 +3,7 @@ package virtx
 import (
 	"net/http"
 	"suse.com/virtx/pkg/hypervisor"
+	"suse.com/virtx/pkg/httpx"
 	"suse.com/virtx/pkg/logger"
 )
 
@@ -12,8 +13,14 @@ func vm_boot(w http.ResponseWriter, r *http.Request) {
 	var (
 		err error
 		uuid string
-		vr VirtxRequest
+		vr httpx.Request
 	)
+	vr, err = httpx.Decode_request_body(r, nil)
+	if (err != nil) {
+		logger.Log(err.Error())
+		http.Error(w, "failed to decode body", http.StatusBadRequest)
+		return
+	}
 	uuid = r.PathValue("uuid")
 	if (uuid == "") {
 		http.Error(w, "could not get uuid", http.StatusBadRequest)

pkg/virtx/vm_create.go
@@ -10,6 +10,7 @@ import (
 	"suse.com/virtx/pkg/logger"
 	"suse.com/virtx/pkg/model"
 	"suse.com/virtx/pkg/vmdef"
+	"suse.com/virtx/pkg/httpx"
 )
 
 
@@ -20,9 +21,9 @@ func vm_create(w http.ResponseWriter, r *http.Request) {
 		err error
 		o openapi.VmCreateOptions
 		xml, uuid string
-		vr VirtxRequest
+		vr httpx.Request
 	)
-	vr, err = http_decode_body(r, &o)
+	vr, err = httpx.Decode_request_body(r, &o)
 	if (err != nil) {
 		logger.Log(err.Error())
 		http.Error(w, "failed to decode body", http.StatusBadRequest)

commit fdecf6ce056066032795717e767ec0da3d9a42a0

virtx: vm_resume: fix crash with proxying vm_resume operations
the vr httpx.Request was left unset for an operation that does proxying.

Fixes: 393ab4efb93bac2def024d5a3bd9f57c01afd437
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/virtx/vm_resume.go
@@ -15,7 +15,7 @@ func vm_resume(w http.ResponseWriter, r *http.Request) {
 		vmdata inventory.Vmdata
 		vr httpx.Request
 	)
-	_, err = httpx.Decode_request_body(r, nil)
+	vr, err = httpx.Decode_request_body(r, nil)
 	if (err != nil) {
 		logger.Log(err.Error())
 		http.Error(w, "failed to decode body", http.StatusBadRequest)