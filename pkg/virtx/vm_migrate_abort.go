a7f1f28d68dcd8900ff1da2f687c9cf4474da33c
Claudio Fontana <cfontana@suse.de>
2025-10-19 14:02:46 +0200

    virtx: vm_migrate_abort: implement via Hypervisor.Abort_migration
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- pkg/virtx/vm_migrate_abort.go
+++ pkg/virtx/vm_migrate_abort.go
@@ -2,16 +2,47 @@ package virtx
 
 import (
 	"net/http"
-	//"encoding/json"
-	//"strings"
-	//"bytes"
-	//"io"
 
-	//"suse.com/virtx/pkg/hypervisor"
-	//"suse.com/virtx/pkg/logger"
-	//"suse.com/virtx/pkg/model"
+	"suse.com/virtx/pkg/hypervisor"
+	"suse.com/virtx/pkg/logger"
+	"suse.com/virtx/pkg/httpx"
+	"suse.com/virtx/pkg/inventory"
 )
 
+
 func vm_migrate_abort(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "not implemented yet", http.StatusNotImplemented)
+	var (
+		err error
+		uuid, host_old string
+		vmdata inventory.Vmdata
+		vr httpx.Request
+	)
+	vr, err = httpx.Decode_request_body(r, nil)
+	if (err != nil) {
+		logger.Log(err.Error())
+		http.Error(w, "failed to decode body", http.StatusBadRequest)
+		return
+	}
+	uuid = r.PathValue("uuid")
+	if (uuid == "") {
+		http.Error(w, "could not get uuid", http.StatusBadRequest)
+		return
+	}
+	vmdata, err = inventory.Get_vm(uuid)
+	if (err != nil) {
+		http.Error(w, "unknown uuid", http.StatusNotFound)
+		return
+	}
+	host_old = vmdata.Runinfo.Host
+	if (http_host_is_remote(host_old)) { /* need to proxy */
+		http_proxy_request(host_old, w, vr);
+		return
+	}
+	err = hypervisor.Abort_migration(uuid)
+	if (err != nil) {
+		logger.Log("Abort_migration failed: %s", err.Error())
+		http.Error(w, "could not abort migration", http.StatusInternalServerError)
+		return
+	}
+	httpx.Do_response(w, http.StatusNoContent, nil)
 }
