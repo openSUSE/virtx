openSUSE/virtx

commit a51dd2cc735db29ed061f569f06ac8185a225b3b

cmd: virtxd: initialize debug with the -D flag
Signed-off-by: Claudio Fontana <cfontana@suse.de>

cmd/virtxd/main.go
@@ -22,6 +22,7 @@ import (
 	"os"
 	"os/signal"
 	"time"
+	"flag"
 
 	"suse.com/virtx/pkg/serfcomm"
 	"suse.com/virtx/pkg/hypervisor"
@@ -34,8 +35,11 @@ var version string = "unknown"
 func main() {
 	var (
 		err error
+		debug *bool
 	)
 	logger.Log("version %s", version)
+	debug = flag.Bool("D", false, "add debug info to logs")
+	logger.Set_debug(*debug)
 
 	/* hypervisor: initialize and start listening to hypervisor events */
 	err = hypervisor.Connect()

commit 5ca4ae8b4352a3bd3cdf82b92486208d144d1f84

cmd: virtxd: fix missing parsing of command line flag
Fixes: a51dd2cc735db29ed061f569f06ac8185a225b3b
Signed-off-by: Claudio Fontana <cfontana@suse.de>

cmd/virtxd/main.go
@@ -39,6 +39,7 @@ func main() {
 	)
 	logger.Log("version %s", version)
 	debug = flag.Bool("D", false, "add debug info to logs")
+	flag.Parse()
 	logger.Set_debug(*debug)
 
 	/* hypervisor: initialize and start listening to hypervisor events */