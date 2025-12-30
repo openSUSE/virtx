openSUSE/virtx

commit a3cf7c087d4d3a8943eaaab0bb940e0694cd18b2

hypervisor: get_domain_stats: get LUN blockinfo stats
get Dev Disks blockinfo in addition to File.

Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/hypervisor/hypervisor.go
@@ -986,6 +986,7 @@ type xmlDisk struct {
 	Device string `xml:"device,attr"`
 	Source struct {
 		File string `xml:"file,attr"`
+		Dev string `xml:"dev,attr"`
 	} `xml:"source"`
 }
 
@@ -1064,16 +1065,25 @@ func get_domain_stats(d *libvirt.Domain, vm *inventory.Vmdata, old *inventory.Vm
 			return err
 		}
 		for _, disk := range xd.Devices.Disks {
-			if (disk.Device == "disk" && disk.Source.File != "") {
-				var blockinfo *libvirt.DomainBlockInfo
-				blockinfo, err = d.GetBlockInfo(disk.Source.File, 0)
-				if (err != nil) {
-					return err
-				}
-				vm.Stats.DiskCapacity += int64(blockinfo.Capacity / MiB)
-				vm.Stats.DiskAllocation += int64(blockinfo.Allocation / MiB)
-				vm.Stats.DiskPhysical += int64(blockinfo.Physical / MiB)
+			var path string
+			if (disk.Device != "disk") {
+				continue
 			}
+			if (disk.Source.File != "") {
+				path = disk.Source.File
+			} else if (disk.Source.Dev != "") {
+				path = disk.Source.Dev
+			} else {
+				continue
+			}
+			var blockinfo *libvirt.DomainBlockInfo
+			blockinfo, err = d.GetBlockInfo(path, 0)
+			if (err != nil) {
+				return err
+			}
+			vm.Stats.DiskCapacity += int64(blockinfo.Capacity / MiB)
+			vm.Stats.DiskAllocation += int64(blockinfo.Allocation / MiB)
+			vm.Stats.DiskPhysical += int64(blockinfo.Physical / MiB)
 		}
 		for _, net := range xd.Devices.Interfaces {
 			if (net.Target.Dev != "") {

commit 31be29a8e249d4a8cecc25d8123710eee3bad7c7

hypervisor: fix missing blockinfo statistics for LUNs
Fixes: a3cf7c087d4d3a8943eaaab0bb940e0694cd18b2
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/hypervisor/hypervisor.go
@@ -1138,7 +1138,7 @@ func get_domain_stats(d *libvirt.Domain, vm *SystemInfoVm, old *SystemInfoVm) er
 		}
 		for _, disk := range xd.Devices.Disks {
 			var path string
-			if (disk.Device != "disk") {
+			if (disk.Device != "disk" && disk.Device != "lun") {
 				continue
 			}
 			if (disk.Source.File != "") {