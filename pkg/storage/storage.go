openSUSE/virtx

commit a7800825c04f4ff6b5bf020fb9c1aeab068541ba

storage: check provisioning type and size even for unmanaged disks
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/storage/storage.go
@@ -31,16 +31,13 @@ import (
 func Create(vm *openapi.Vmdef, old *openapi.Vmdef) error {
 	var err error
 	for _, disk := range vmdef.Disks(vm) {
-		if (!storage_is_managed_disk(disk)) {
-			continue
-		}
 		if (old != nil && vmdef.Has_path(old, disk.Path)) {
 			continue
 		}
-		if (disk.Prov == openapi.DISK_PROV_NONE) {
-			err = storage_detect_prov(disk)
-		} else {
+		if (storage_is_managed_disk(disk) && disk.Prov != openapi.DISK_PROV_NONE) {
 			err = storage_create_disk(disk)
+		} else {
+			err = storage_detect_prov(disk)
 		}
 		if (err != nil) {
 			return err
@@ -56,10 +53,10 @@ func Create(vm *openapi.Vmdef, old *openapi.Vmdef) error {
 func Delete(vm *openapi.Vmdef, new *openapi.Vmdef) error {
 	var err error
 	for _, disk := range vmdef.Disks(vm) {
-		if (!storage_is_managed_disk(disk)) {
+		if (new != nil && vmdef.Has_path(new, disk.Path)) {
 			continue
 		}
-		if (new != nil && vmdef.Has_path(new, disk.Path)) {
+		if (!storage_is_managed_disk(disk)) {
 			continue
 		}
 		err = storage_delete_disk(disk)

commit b19e0103a25c0a3dc428d9e26e1c770435875ef5

storage: storage_detect_prov: fix unhandled device type for CDROM
Fixes: a7800825c04f4ff6b5bf020fb9c1aeab068541ba
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/storage/storage.go
@@ -85,6 +85,7 @@ func storage_create_disk(disk *openapi.Disk) error {
 func storage_detect_prov(disk *openapi.Disk) error {
 	switch (disk.Device) {
 	case openapi.DEVICE_DISK:
+	case openapi.DEVICE_CDROM:
 		return vdisk_detect_prov(disk)
 	default:
 		return errors.New("invalid disk device")