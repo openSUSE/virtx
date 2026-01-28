openSUSE/virtx

commit b19e0103a25c0a3dc428d9e26e1c770435875ef5

storage: storage_detect_prov: fix unhandled device type for CDROM
Fixes: a7800825c04f4ff6b5bf020fb9c1aeab068541ba
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/storage/storage.go
@@ -85,6 +85,7 @@ func storage_create_disk(disk *openapi.Disk) error {
 func storage_detect_prov(disk *openapi.Disk) error {
 	switch (disk.Device) {
 	case openapi.DEVICE_DISK:
+	case openapi.DEVICE_CDROM:
 		return vdisk_detect_prov(disk)
 	default:
 		return errors.New("invalid disk device")

commit f5d689f381cc5861089175274ff39134feaa8516

storage: fix missing fallthrough
Golang vs C

Fixes: b19e0103a25c0a3dc428d9e26e1c770435875ef5
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/storage/storage.go
@@ -85,6 +85,7 @@ func storage_create_disk(disk *openapi.Disk) error {
 func storage_detect_prov(disk *openapi.Disk) error {
 	switch (disk.Device) {
 	case openapi.DEVICE_DISK:
+		fallthrough
 	case openapi.DEVICE_CDROM:
 		return vdisk_detect_prov(disk)
 	default: