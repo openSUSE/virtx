openSUSE/virtx

commit 6a9fd6c9acebe86691dec4fde51d9a88fce95ae6

hypervisor: do not return from the system_loop_info on error
instead report the error and continue

Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/hypervisor/hypervisor.go
@@ -232,7 +232,8 @@ func system_info_loop(seconds int) error {
 	for range ticker.C {
 		err = get_system_info(&si, &old)
 		if (err != nil) {
-			return err
+			logger.Log("system_info_loop: failed to get_system_info: %s", err.Error())
+			continue
 		}
 		hv.system_info_ch <- si
 		old = si

commit e60430b3b83bbe3c46531494c9e90489099b6e6e

hypervisor: systeminfo: fix libvirt reconnection
This is a partial revert of 6a9fd6c9acebe86691dec4fde51d9a88fce95ae6,
that ended up disabling the reconnection code completely.

This is still imperfect and may need revising later,
in terms of determining which errors exactly warrant a libvirt reconnection,
and in general reviewing all error paths.

Fixes: 6a9fd6c9acebe86691dec4fde51d9a88fce95ae6
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/hypervisor/hypervisor.go
@@ -199,23 +199,13 @@ func init_system_info_loop() {
 		time.Sleep(time.Duration(1) * time.Second)
 	}
 	for {
-		var (
-			err error
-			libvirt_err libvirt.Error
-			ok bool
-		)
+		var err error
 		err = system_info_loop(libvirt_system_info_seconds)
-		libvirt_err, ok = err.(libvirt.Error)
-		if (ok) {
-			if (libvirt_err.Level >= libvirt.ERR_ERROR) {
-				logger.Log(err.Error())
-				logger.Debug("reconnect, attempt every %d seconds...", libvirt_reconnect_seconds)
-				for ; err != nil; err = Connect() {
-					time.Sleep(time.Duration(libvirt_reconnect_seconds) * time.Second)
-				}
-			}
-		} else {
-			logger.Log(err.Error())
+		/* we should from system_info_loop only if there is a libvirt error that requires reconnection */
+		/* assert(err != nil) */
+		logger.Debug("reconnect, attempt every %d seconds...", libvirt_reconnect_seconds)
+		for ; err != nil; err = Connect() {
+			time.Sleep(time.Duration(libvirt_reconnect_seconds) * time.Second)
 		}
 	}
 	logger.Debug("init_system_info_loop: Exiting")

pkg/hypervisor/systeminfo.go
@@ -91,6 +91,8 @@ func system_info_loop(seconds int) error {
 		si SystemInfo
 		err error
 		ticker *time.Ticker
+		libvirt_err libvirt.Error
+		ok bool
 	)
 	logger.Debug("system_info_loop starting...")
 	defer logger.Debug("system_info_loop exit")
@@ -99,7 +101,11 @@ func system_info_loop(seconds int) error {
 
 	si, err = get_system_info()
 	if (err != nil) {
-		return err
+		logger.Log("system_info_loop: failed to get_system_info: %s", err.Error())
+		libvirt_err, ok = err.(libvirt.Error)
+		if (ok && libvirt_err.Level >= libvirt.ERR_ERROR) {
+			return err
+		}
 	}
 	hv.m.Lock()
 	hv.uuid = si.Host.Uuid
@@ -114,6 +120,10 @@ func system_info_loop(seconds int) error {
 		si, err = get_system_info()
 		if (err != nil) {
 			logger.Log("system_info_loop: failed to get_system_info: %s", err.Error())
+			libvirt_err, ok = err.(libvirt.Error)
+			if (ok && libvirt_err.Level >= libvirt.ERR_ERROR) {
+				return err
+			}
 			continue
 		}
 		hv.system_info_ch <- si