8bb86d7d7815fb938befe04c7aa6b68fb324256d
Claudio Fontana <cfontana@suse.de>
2026-02-11 23:02:21 +0100

    cmd: virtx: vm_list: show age of VM information
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- cmd/virtx/vm_list.go
+++ cmd/virtx/vm_list.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"suse.com/virtx/pkg/model"
+	"suse.com/virtx/pkg/ts"
 )
 
 func vm_list_req() {
@@ -14,10 +15,10 @@ func vm_list_req() {
 
 func vm_list(list *openapi.VmList) {
 
-	fmt.Fprintf(virtx.w, "UUID\tNAME\tHOST\tVLANID\t \tSTATE\n")
+	fmt.Fprintf(virtx.w, "UUID\tNAME\tHOST\tVLANID\t \tSTATE\tAGE\n")
 
 	for _, item := range (list.Items) {
-		fmt.Fprintf(virtx.w, "%s\t%s\t%s\t%6d\t%v\t%s\n", item.Uuid, item.Fields.Name, item.Fields.Host,
-			item.Fields.Vlanid, item.Fields.Custom, item.Fields.Runstate)
+		fmt.Fprintf(virtx.w, "%s\t%s\t%s\t%6d\t%v\t%s\t%s\n", item.Uuid, item.Fields.Name, item.Fields.Host,
+			item.Fields.Vlanid, item.Fields.Custom, item.Fields.Runstate, ts.Since(item.Fields.Ts))
 	}
 }
