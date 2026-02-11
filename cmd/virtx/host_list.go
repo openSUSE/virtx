f2952026d04c047a87f77a71dd967dcbfaa1055e
Claudio Fontana <cfontana@suse.de>
2026-02-11 22:58:17 +0100

    cmd: virtx: host_list: show age of host information
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- cmd/virtx/host_list.go
+++ cmd/virtx/host_list.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"suse.com/virtx/pkg/model"
+	"suse.com/virtx/pkg/ts"
 )
 
 func host_list_req() {
@@ -14,14 +15,14 @@ func host_list_req() {
 
 func host_list(list *openapi.HostList) {
 
-	fmt.Fprintf(virtx.w, "UUID\tNAME\tOS\tVERSION\tCPU\tVENDOR\tMODEL\tTHREADS\t MEM_AVL_VM\t HPG_AVL_VM\tSTATE\n")
+	fmt.Fprintf(virtx.w, "UUID\tNAME\tOS\tVERSION\tCPU\tVENDOR\tMODEL\tTHREADS\t MEM_AVL_VM\t HPG_AVL_VM\tSTATE\tAGE\n")
 
 	for _, item := range (list.Items) {
-		fmt.Fprintf(virtx.w, "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%7d\t%7d MiB\t%7d MiB\t%s\n",
+		fmt.Fprintf(virtx.w, "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%7d\t%7d MiB\t%7d MiB\t%s\t%s\n",
 			item.Uuid, item.Fields.Name, item.Fields.Osid, item.Fields.Osv,
 			item.Fields.Cpuarch.Arch, item.Fields.Cpuarch.Vendor, item.Fields.Cpudef.Model,
 			item.Fields.Cpudef.Nodes * item.Fields.Cpudef.Sockets * item.Fields.Cpudef.Cores * item.Fields.Cpudef.Threads,
 			item.Fields.Memoryavailable, item.Fields.Hpavailable,
-			item.Fields.Hoststate)
+			item.Fields.Hoststate, ts.Since(item.Fields.Ts))
 	}
 }
