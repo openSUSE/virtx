86e4a26c660d43611f135eeacfb65041f2815930
Claudio Fontana <cfontana@suse.de>
2026-02-11 21:40:18 +0100

    cmd: virtx: vm_list: move VM runstate to the last column
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- cmd/virtx/vm_list.go
+++ cmd/virtx/vm_list.go
@@ -14,10 +14,10 @@ func vm_list_req() {
 
 func vm_list(list *openapi.VmList) {
 
-	fmt.Fprintf(virtx.w, "UUID\tNAME\tHOST\tSTATE\tVLANID\t \n")
+	fmt.Fprintf(virtx.w, "UUID\tNAME\tHOST\tVLANID\t \tSTATE\n")
 
 	for _, item := range (list.Items) {
-		fmt.Fprintf(virtx.w, "%s\t%s\t%s\t%s\t%6d\t%v\n", item.Uuid, item.Fields.Name, item.Fields.Host,
-			item.Fields.Runstate, item.Fields.Vlanid, item.Fields.Custom)
+		fmt.Fprintf(virtx.w, "%s\t%s\t%s\t%6d\t%v\t%s\n", item.Uuid, item.Fields.Name, item.Fields.Host,
+			item.Fields.Vlanid, item.Fields.Custom, item.Fields.Runstate)
 	}
 }
