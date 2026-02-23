62f7ce83a63780bb64ca0db1749a455cb8540c7c
Claudio Fontana <cfontana@suse.de>
2026-02-23 13:40:25 +0100

    constants: add LOCK_DIR and LOCK_SPACE in preparation for lockman
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -27,7 +27,10 @@ const (
 const (
 	REG_DIR = "/vms/xml/"
 	DS_DIR = "/vms/ds/"
+	LOCK_DIR = "/vms/lock/"
+	LOCK_SPACE = "__VIRTX__DISKS__"
 	DEV_DIR = "/dev/"
+
 	HTTP_MAX_BODY_LEN = 1048576
 	VM_NAME_MAX = 32
 	NET_NAME_MAX = 32
