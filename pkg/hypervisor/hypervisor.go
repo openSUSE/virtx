openSUSE/virtx

commit b5581c80a7c7fcea088f24e3555960338c3d23b3

hypervisor: lifecycle_cb: add special handling for libvirt.DOMAIN_EVENT_DEFINED
when such event is generated (on new VM creation), the effect is to send to serfcomm
a minimal inventory.VmEvent, with the assumption that the Vm is already in the inventory,
since this is "just" a state change.

However when a new VM is defined, the Vm will NOT be in the inventory, resulting in
the Vm not being added to the inventory until the first systeminfo loop finds the VM
(it can take up to 15 seconds currently).

Serf is eventually consistent anyway, so it is still possible to get for example a
404 just after creating a VM, so client behavior must still consider appropriate error
handling (retries with backoff for example).

But this can reduce the time spent waiting for the server by many seconds.

---

Note: it is possible that the whole inventory.Update_vm_state vs inventory.Update_vm
needs some refactoring to make this less clunky, but this seems a good enough solution
at least for now.

Reported-by: Lin Ma <lma@suse.de>
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/hypervisor/hypervisor.go
@@ -167,7 +167,22 @@ func lifecycle_cb(_ *libvirt.Connect, d *libvirt.Domain, e *libvirt.DomainEventL
 	if (err != nil) {
 		logger.Log("lifecycle_cb: event %d: %s:", e.Event, err.Error())
 	}
-	if (state != openapi.RUNSTATE_NONE) {
+	if (e.Event == libvirt.DOMAIN_EVENT_DEFINED) {
+		var (
+			si SystemInfo
+			vm SystemInfoVm
+		)
+		si.Host.Uuid = "" /* not necessary, but for documentation, do not send Host Data */
+		si.Vms = make(SystemInfoVms)
+		vm.Name, vm.Uuid, vm.Runinfo.Runstate, vm.Runinfo.Host = name, uuid, state, hv.uuid
+		err = get_domain_stats(d, &vm, nil, &si.imm)
+		if (err != nil) {
+			logger.Log("lifecycle_cb: failed to get_domain_stats for uuid %s", uuid)
+		} else {
+			si.Vms[vm.Uuid] = vm
+			hv.system_info_ch <- si
+		}
+	} else if (state != openapi.RUNSTATE_NONE) {
 		logger.Debug("[VmEvent] %s/%s: %v state: %d", name, uuid, e, state)
 		_ = name
 		hv.vm_event_ch <- inventory.VmEvent{ Uuid: uuid, Host: hv.uuid, State: state, Ts: ts.Now() }

pkg/serfcomm/serfcomm.go
@@ -237,13 +237,16 @@ func send_system_info(ch <-chan hypervisor.SystemInfo) {
 			/* do nothing with the systeminfo if we are not connected */
 			continue
 		}
-		err = update_tags(&si.Host)
-		if (err != nil) {
-			logger.Log("update_tags: " + err.Error())
-		}
-		err = send_host_info(&si.Host)
-		if (err != nil) {
-			logger.Log("send_host_info: " + err.Error())
+		if (si.Host.Uuid != "") {
+			/* we have a full System Info with Host Information and all VMs */
+			err = update_tags(&si.Host)
+			if (err != nil) {
+				logger.Log("update_tags: " + err.Error())
+			}
+			err = send_host_info(&si.Host)
+			if (err != nil) {
+				logger.Log("send_host_info: " + err.Error())
+			}
 		}
 		for _, vm := range si.Vms {
 			err = send_vm_data(&vm.Vmdata)

commit 55c97a1902bb4c67edfe8bea13548606f6c51859

hypervisor: lifecycle_cb: add missing Ts when handling DOMAIN_EVENT_DEFINED
Fixes: b5581c80a7c7fcea088f24e3555960338c3d23b3
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/hypervisor/hypervisor.go
@@ -174,7 +174,7 @@ func lifecycle_cb(_ *libvirt.Connect, d *libvirt.Domain, e *libvirt.DomainEventL
 		)
 		si.Host.Uuid = "" /* not necessary, but for documentation, do not send Host Data */
 		si.Vms = make(SystemInfoVms)
-		vm.Name, vm.Uuid, vm.Runinfo.Runstate, vm.Runinfo.Host = name, uuid, state, hv.uuid
+		vm.Name, vm.Uuid, vm.Runinfo.Runstate, vm.Runinfo.Host, vm.Ts = name, uuid, state, hv.uuid, ts.Now()
 		err = get_domain_stats(d, &vm, nil, &si.imm)
 		if (err != nil) {
 			logger.Log("lifecycle_cb: failed to get_domain_stats for uuid %s", uuid)