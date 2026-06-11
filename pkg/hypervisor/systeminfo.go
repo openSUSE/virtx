openSUSE/virtx

commit 89b87f155a7541b8c7d0a3def7cc91cf7d4eac20

hypervisor/domain: change get_domain_info() to return an inventory.VmEvent
the full signature becomes:

func get_domain_info(d *libvirt.Domain) (inventory.VmEvent, string, error)

We return a VmEvent (without the .Ts field filled),
a string (the name of the domain),
and an error (!= nil on error).

Having a VmEvent makes it easier to initialize structs or pass to channels
that expect a VmEvent.

The previous signature was:

func get_domain_info(d *libvirt.Domain) (string, string, openapi.Vmrunstate, error)

Where we returned name, uuid, runstate, error.

Notably we did not return the host UUID (which is just our host UUID),
whereas now we also fill the VmEvent .Host field with our host UUID.

Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/hypervisor/systeminfo.go
@@ -227,16 +227,15 @@ func system_info_get() (SystemInfo, error) {
 			oldvm SystemInfoVm
 			present bool
 		)
-		vm.Name, vm.Uuid, vm.Runstate, err = get_domain_info(&d)
+		vm.Vmdata.VmEvent, vm.Name, err = get_domain_info(&d)
 		if (err != nil) {
 			logger.Log("could not get_domain_info: %s", err.Error())
 			continue
 		}
+		vm.Ts = host.Ts
 		if (hv.si != nil) {
 			oldvm, present = hv.si.Vms[vm.Uuid]
 		}
-		vm.Host = host.Uuid
-		vm.Ts = host.Ts
 		if (present) {
 			err = get_domain_stats(&d, &vm, &oldvm, &si.imm)
 		} else {

pkg/hypervisor/domain.go
@@ -27,25 +27,34 @@ import (
 	"suse.com/virtx/pkg/model"
 	"suse.com/virtx/pkg/logger"
 	"suse.com/virtx/pkg/vmreg"
+	"suse.com/virtx/pkg/inventory"
 	"suse.com/virtx/pkg/ts"
 )
 
-/* get basic information about a Domain */
-func get_domain_info(d *libvirt.Domain) (string, string, openapi.Vmrunstate, error) {
+/*
+ * get basic information about a Domain.
+ *
+ * Note that we return an inventory.VmEvent for convenience, but the timestamp is not set
+ * (ve.Ts). This is left to the caller, because in most cases for a systeminfo collection
+ * we want to keep the same timestamp for all related systeminfovms, equal to the host one.
+ * This is easier for debugging.
+ * We also want to avoid tight loops calling gettimeofday() just to update the Ts for all
+ * the VMs, if we have a large number of them.
+ */
+func get_domain_info(d *libvirt.Domain) (inventory.VmEvent, string, error) {
 	/* assert (hv.m.IsRLocked) */
 	var (
+		ve inventory.VmEvent
 		name string
-		uuid string
 		reason int
 		state libvirt.DomainState
 		err error
-		enum_state openapi.Vmrunstate = openapi.RUNSTATE_NONE
 	)
 	name, err = d.GetMetadata(libvirt.DOMAIN_METADATA_TITLE, "", libvirt.DOMAIN_AFFECT_CONFIG)
 	if (err != nil) {
 		goto out
 	}
-	uuid, err = d.GetUUIDString()
+	ve.Uuid, err = d.GetUUIDString()
 	if (err != nil) {
 		goto out
 	}
@@ -55,53 +64,54 @@ func get_domain_info(d *libvirt.Domain) (string, string, openapi.Vmrunstate, err
 	}
 	logger.Debug("get_domain_info: state %d, reason %d", state, reason)
 	switch (state) {
-	//case libvirt.DOMAIN_NOSTATE: /* leave enum_state RUNSTATE_NONE */
+	//case libvirt.DOMAIN_NOSTATE: /* leave ve.Runstate RUNSTATE_NONE */
 	case libvirt.DOMAIN_RUNNING:
-		enum_state = openapi.RUNSTATE_RUNNING
+		ve.Runstate = openapi.RUNSTATE_RUNNING
 	case libvirt.DOMAIN_BLOCKED: /* should be Xen only IIUC */
 		logger.Log("XXX DOMAIN_BLOCKED encountered XXX")
-		enum_state = openapi.RUNSTATE_PAUSED
+		ve.Runstate = openapi.RUNSTATE_PAUSED
 	case libvirt.DOMAIN_PAUSED:
 		switch (reason) {
 		case int(libvirt.DOMAIN_PAUSED_MIGRATION): /* paused for offline migration */
-			enum_state = openapi.RUNSTATE_MIGRATING
+			ve.Runstate = openapi.RUNSTATE_MIGRATING
 		case int(libvirt.DOMAIN_PAUSED_SHUTTING_DOWN):
-			enum_state = openapi.RUNSTATE_TERMINATING
+			ve.Runstate = openapi.RUNSTATE_TERMINATING
 		case int(libvirt.DOMAIN_PAUSED_CRASHED):
-			enum_state = openapi.RUNSTATE_CRASHED
+			ve.Runstate = openapi.RUNSTATE_CRASHED
 		case int(libvirt.DOMAIN_PAUSED_STARTING_UP):
-			enum_state = openapi.RUNSTATE_STARTUP
+			ve.Runstate = openapi.RUNSTATE_STARTUP
 		default:
-			enum_state = openapi.RUNSTATE_PAUSED
+			ve.Runstate = openapi.RUNSTATE_PAUSED
 		}
 	case libvirt.DOMAIN_SHUTDOWN:
-		enum_state = openapi.RUNSTATE_TERMINATING
+		ve.Runstate = openapi.RUNSTATE_TERMINATING
 	case libvirt.DOMAIN_SHUTOFF:
 		switch (reason) {
 		case int(libvirt.DOMAIN_SHUTOFF_CRASHED):
-			enum_state = openapi.RUNSTATE_CRASHED
+			ve.Runstate = openapi.RUNSTATE_CRASHED
 		case int(libvirt.DOMAIN_SHUTOFF_MIGRATED):
 			/* XXX I have never seen this yet in my migration tests XXX */
 			logger.Log("XXX DOMAIN_SHUTOFF_MIGRATED encountered XXX")
-			enum_state = openapi.RUNSTATE_DELETED
+			ve.Runstate = openapi.RUNSTATE_DELETED
 		case int(libvirt.DOMAIN_SHUTOFF_DESTROYED):
 			_ = oplog_complete(d, openapi.OpVmShutdown, "forced shutdown")
-			enum_state = openapi.RUNSTATE_POWEROFF
+			ve.Runstate = openapi.RUNSTATE_POWEROFF
 		case int(libvirt.DOMAIN_SHUTOFF_SHUTDOWN):
 			_ = oplog_complete(d, openapi.OpVmShutdown, "graceful shutdown")
 			fallthrough
 		default:
-			enum_state = openapi.RUNSTATE_POWEROFF
+			ve.Runstate = openapi.RUNSTATE_POWEROFF
 		}
 	case libvirt.DOMAIN_CRASHED:
-		enum_state = openapi.RUNSTATE_CRASHED
+		ve.Runstate = openapi.RUNSTATE_CRASHED
 	case libvirt.DOMAIN_PMSUSPENDED:
-		enum_state = openapi.RUNSTATE_PMSUSPENDED
+		ve.Runstate = openapi.RUNSTATE_PMSUSPENDED
 	default:
 		logger.Log("Unhandled state %d, reason %d", state, reason)
 	}
+	ve.Host = hv.uuid
 out:
-	return name, uuid, enum_state, err
+	return ve, name, err
 }
 
 func Define_domain(xml string, uuid string) error {

pkg/hypervisor/hypervisor.go
@@ -133,8 +133,8 @@ func Shutdown() {
 func lifecycle_cb(_ *libvirt.Connect, d *libvirt.Domain, e *libvirt.DomainEventLifecycle) {
 	/* e.Detail: see all DomainEvent*DetailType types */
 	var (
-		name, uuid string
-		state openapi.Vmrunstate
+		ve inventory.VmEvent
+		name string
 		persistent bool
 		err error
 	)
@@ -147,12 +147,14 @@ func lifecycle_cb(_ *libvirt.Connect, d *libvirt.Domain, e *libvirt.DomainEventL
 
 	if (e.Event == libvirt.DOMAIN_EVENT_UNDEFINED) {
 		/* VM has been DELETED */
-		uuid, err = d.GetUUIDString()
+		ve.Uuid, err = d.GetUUIDString()
 		if (err != nil) {
 			logger.Log("lifecycle_cb: GetUUIDString error: %s", err.Error())
 			return
 		}
-		state = openapi.RUNSTATE_DELETED
+		ve.Runstate = openapi.RUNSTATE_DELETED
+		ve.Host = hv.uuid
+		ve.Ts = ts.Now()
 	} else {
 		persistent, err = d.IsPersistent()
 		if (err != nil) {
@@ -162,7 +164,8 @@ func lifecycle_cb(_ *libvirt.Connect, d *libvirt.Domain, e *libvirt.DomainEventL
 		if (!persistent) {
 			return /* ignore transient domains (ongoing migrations) */
 		}
-		name, uuid, state, err = get_domain_info(d)
+		ve, name, err = get_domain_info(d)
+		ve.Ts = ts.Now()
 	}
 	if (err != nil) {
 		logger.Log("lifecycle_cb: event %d: %s:", e.Event, err.Error())
@@ -174,18 +177,18 @@ func lifecycle_cb(_ *libvirt.Connect, d *libvirt.Domain, e *libvirt.DomainEventL
 		)
 		si.Host.Uuid = "" /* not necessary, but for documentation, do not send Host Data */
 		si.Vms = make(SystemInfoVms)
-		vm.Name, vm.Uuid, vm.Runstate, vm.Host, vm.Ts = name, uuid, state, hv.uuid, ts.Now()
+		vm.Vmdata.VmEvent = ve
+		vm.Name = name
 		err = get_domain_stats(d, &vm, nil, &si.imm)
 		if (err != nil) {
-			logger.Log("lifecycle_cb: failed to get_domain_stats for uuid %s", uuid)
+			logger.Log("lifecycle_cb: failed to get_domain_stats for uuid %s", ve.Uuid)
 		} else {
 			si.Vms[vm.Uuid] = vm
 			hv.system_info_ch <- si
 		}
-	} else if (state != openapi.RUNSTATE_NONE) {
-		logger.Debug("[VmEvent] %s/%s: %v state: %d", name, uuid, e, state)
-		_ = name
-		hv.vm_event_ch <- inventory.VmEvent{ Uuid: uuid, Host: hv.uuid, Runstate: state, Ts: ts.Now() }
+	} else if (ve.Runstate != openapi.RUNSTATE_NONE) {
+		logger.Debug("[VmEvent] %s/%s: %v state: %d", name, ve.Uuid, e, ve.Runstate)
+		hv.vm_event_ch <- ve
 	}
 	/* check for the need to remove a cloudinit disk resource file */
 	if ((e.Event == libvirt.DOMAIN_EVENT_STOPPED && e.Detail != int(libvirt.DOMAIN_EVENT_STOPPED_MIGRATED)) ||
@@ -213,7 +216,7 @@ func lifecycle_cb(_ *libvirt.Connect, d *libvirt.Domain, e *libvirt.DomainEventL
 		 *   Needs to be tested.
 		 *   XXX
 		 */
-		err = cloudinit.Delete_disk(uuid)
+		err = cloudinit.Delete_disk(ve.Uuid)
 		if (err != nil) {
 			logger.Log("lifecycle_cb: cloudinit : %s", err)
 		}

commit 56d41bc31cc105be684a1b72babfb5b6d62fa2a3

hypervisor/systeminfo: fix missing Uuid in the first VmInfo events
set the Uuid and Arch as soon as we get it from libvirt.
We need Uuid set inside system_info_get() before we start sending VmInfo events
because otherwise we will send them with an empty host Uuid field.

This bug has been introduced with the refactoring with commit
("hypervisor/domain: change get_domain_info() to return an inventory.VmEvent")

Fixes: 89b87f155a7541b8c7d0a3def7cc91cf7d4eac20
Signed-off-by: Claudio Fontana <cfontana@suse.de>

pkg/hypervisor/systeminfo.go
@@ -119,8 +119,6 @@ func system_info_loop(seconds int) error {
 		}
 	}
 	hv.m.Lock()
-	hv.uuid = si.Host.Uuid
-	arch.Set(si.Host.Cpuarch.Arch)
 	check_vmreg(hv.uuid, &si)
 	hv.m.Unlock()
 
@@ -182,6 +180,10 @@ func system_info_get() (SystemInfo, error) {
 		if (err != nil) {
 			goto out
 		}
+		/***** SET THE HYPERVISOR UUID AND ARCHITECTURE *****/
+		hv.uuid = si.imm.caps.Host.UUID
+		arch.Set(si.imm.caps.Host.CPU.Arch)
+		/****************************************************/
 	} else {
 		si.imm = hv.si.imm
 	}