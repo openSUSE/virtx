9ed8184308377fc4014ffcc2187e7306acc0a7b2
Claudio Fontana <cfontana@suse.de>
2026-06-18 00:22:31 +0200

    updated copyright headers
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- pkg/storage/lun.go
+++ pkg/storage/lun.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2025 SUSE LLC
+ * Copyright (c) 2024-2026 SUSE LLC
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
