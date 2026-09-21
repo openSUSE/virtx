7245dd7be485bd05d394b9f7e4373ab883cbdeb5
Claudio Fontana <cfontana@suse.de>
2026-09-21 21:45:31 +0200

    hypervisor/resume: new module, extracted from domain.go
    Signed-off-by: Claudio Fontana <cfontana@suse.de>

--- pkg/hypervisor/domain.go
+++ pkg/hypervisor/domain.go
@@ -188,42 +188,6 @@ func Dumpxml(uuid string) (string, error) {
 	return xml, nil
 }
 
-func Resume_domain(uuid string) error {
-	var (
-		err error
-		conn *libvirt.Connect
-		domain *libvirt.Domain
-		op openapi.Operation = openapi.OpVmResume
-	)
-	conn, err = libvirt.NewConnect(LIBVIRT_URI)
-	if (err != nil) {
-		return err
-	}
-	defer conn.Close()
-	domain, err = conn.LookupDomainByUUIDString(uuid)
-	if (err != nil) {
-		return err
-	}
-	defer domain.Free()
-	oplog_off, oplog_err := oplog.Start(uuid, op, "")
-	defer func() {
-		if (oplog_err != nil) {
-			logger.Log("Resume_domain: oplog: %s", oplog_err.Error())
-		}
-	}()
-	err = domain.Resume()
-	if (err != nil) {
-		if (oplog_err == nil) {
-			oplog_err = oplog.End(uuid, op, openapi.OPERATION_FAILED, err.Error(), oplog_off)
-		}
-		return err
-	}
-	if (oplog_err == nil) {
-		oplog_err = oplog.End(uuid, op, openapi.OPERATION_COMPLETED, "", oplog_off)
-	}
-	return nil
-}
-
 func Shutdown_domain(uuid string, force int16) error {
 	var (
 		err error
